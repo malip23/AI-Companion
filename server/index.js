caa4253ff9ee5ec470988020dda6d88a637d005d
malip23 <malip23@vt.edu>
2026-03-28 15:25:43 -0400

    updates for keys

--- server/index.js
+++ server/index.js
@@ -1,4 +1,8 @@
-import 'dotenv/config';
+import dotenv from 'dotenv';
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+dotenv.config({ path: resolve(__dirname, '../.env') });
 import express from 'express';
 import webhookRouter from './webhookHandler.js';
 import { findByUserId } from './conversationStore.js';
